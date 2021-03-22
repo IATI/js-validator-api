2d502204afb3df1d03cf9b21eaf27526dd8287e9
nosvalds <niko@devinit.org>
2021-03-22 13:39:07 +0000

    basic XML to JSON parse with tests

---
+++ pub-validate-post/index.js
@@ -0,0 +1,33 @@
+const { validate } = require('../services/validationService.js');
+const { appInsights } = require('../config/appInsights');
+
+const pubValidatePost = async (context, req) => {
+    await validate(context, req);
+};
+
+// Default export wrapped with Application Insights FaaS context propagation
+exports.index = async (context, req) => {
+    // Start an AI Correlation Context using the provided Function context
+    const correlationContext = appInsights.startOperation(context, req);
+
+    // Wrap the Function runtime with correlationContext
+    return appInsights.wrapWithCorrelationContext(async () => {
+        const startTime = Date.now(); // Start trackRequest timer
+
+        // Run the Function
+        await pubValidatePost(context, req);
+
+        // Track Request on completion
+        appInsights.defaultClient.trackRequest({
+            name: `${context.req.method} ${context.req.url}`,
+            resultCode: context.res.status,
+            success: true,
+            url: req.url,
+            duration: Date.now() - startTime,
+            id: correlationContext.operation.parentId,
+        });
+        appInsights.defaultClient.flush();
+    }, correlationContext)();
+};
+
+exports.pubValidatePost = pubValidatePost;
---
+++ services/validationService.js
@@ -0,0 +1,41 @@
+const xml2js = require('xml2js');
+const { client, getStartTime, getElapsedTime } = require('../config/appInsights');
+
+exports.validate = async (context, req) => {
+    const { body } = req;
+    const state = {
+        fileSize: '',
+    };
+
+    // Metric: File Size (Mb)
+    state.fileSize = Number(Buffer.byteLength(body) / 1000000).toFixed(4);
+
+    client.trackMetric({ name: 'File Size (Mb)', value: state.fileSize });
+    context.log({ name: 'File Size (Mb)', value: state.fileSize });
+
+    try {
+        const parseStart = getStartTime();
+        const json = await xml2js.parseStringPromise(body);
+
+        state.parseTime = getElapsedTime(parseStart);
+        context.log({ name: 'Parse Time (s)', value: state.parseTime });
+
+        context.res = {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: json,
+        };
+        return;
+    } catch (error) {
+        context.log(error);
+
+        context.res = {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+            body: {
+                feedback: 'unhandled server error please contact the iati technical team',
+                error,
+            },
+        };
+    }
+};
