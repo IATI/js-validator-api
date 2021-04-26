a9732c4c474f36af6e28f41c187833724573ab88
nosvalds <niko@devinit.org>
2021-04-26 16:44:50 +0100

    basic cache of OrgIds

--- identifiers/fetchIdenitifiers.js
+++ identifiers/fetchIdenitifiers.js
@@ -1,12 +1,18 @@
 const fetch = require('node-fetch');
 
+let orgIds = '';
+
 exports.getOrgIds = async () => {
-    const res = await fetch('http://org-id.guide/download.json');
-    const fullOrgIdInfo = await res.json();
-    return fullOrgIdInfo.lists.reduce((acc, orgId) => {
-        if (orgId.confirmed) {
-            acc.add(orgId.code);
-        }
-        return acc;
-    }, new Set());
+    if (orgIds === '') {
+        console.log('Fetching OrgIds');
+        const res = await fetch('http://org-id.guide/download.json');
+        const fullOrgIdInfo = await res.json();
+        orgIds = fullOrgIdInfo.lists.reduce((acc, orgId) => {
+            if (orgId.confirmed) {
+                acc.add(orgId.code);
+            }
+            return acc;
+        }, new Set());
+    }
+    return orgIds;
 };
