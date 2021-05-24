a556ce2ffd82f785dddf73b69f2adbaad819bb98
nosvalds <niko@devinit.org>
2021-05-24 16:11:36 +0100

    change prefix for startsWith to always be an Array of strings

--- services/rulesValidator.js
+++ services/rulesValidator.js
@@ -299,7 +299,7 @@ class Rules {
 
     startsWith(oneCase) {
         // ORG ID Prefix case
-        if (oneCase.prefix === 'ORG-ID-PREFIX') {
+        if (oneCase.prefix.length === 1 && oneCase.prefix[0] === 'ORG-ID-PREFIX') {
             return this.pathMatchesText.every((pathMatchText) => {
                 // Get prefix as everything left of the 2nd "-"
                 const split = pathMatchText.split('-');
