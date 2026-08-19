40de213eda55f970ca5893cf35f38a00c5991adb
arobson-ods <andrew.robson@opendataservices.coop>
2026-08-19 09:59:58 +0100

    style: fix eslint errors on the github api helpers

--- utils/utils.js
+++ utils/utils.js
@@ -9,28 +9,21 @@ import config from '../config/config.js';
 const GITHUB_API = 'https://api.github.com';
 
 const getFileBySha = async (owner, repo, sha, filePath) => {
-    const headers = { Accept: "application/vnd.github.raw+json" };
-    if (
-        config.GITHUB_OAUTH_APP_CLIENT_ID &&
-        config.GITHUB_OAUTH_APP_CLIENT_SECRET
-    ) {
-        headers["Authorization"] =
-            "Basic " +
-            Buffer.from(
-                config.GITHUB_OAUTH_APP_CLIENT_ID +
-                ":" +
-                config.GITHUB_OAUTH_APP_CLIENT_SECRET,
-            ).toString("base64");
+    const headers = { Accept: 'application/vnd.github.raw+json' };
+    if (config.GITHUB_OAUTH_APP_CLIENT_ID && config.GITHUB_OAUTH_APP_CLIENT_SECRET) {
+        headers.Authorization = `Basic ${Buffer.from(
+            `${config.GITHUB_OAUTH_APP_CLIENT_ID}:${config.GITHUB_OAUTH_APP_CLIENT_SECRET}`,
+        ).toString('base64')}`;
     }
     const res = await fetch(
         `${GITHUB_API}/repos/${owner}/${repo}/contents/${filePath}?ref=${sha}`,
         {
-            method: "GET",
-            headers: headers,
+            method: 'GET',
+            headers,
         },
     );
     // This can be useful to check auth. You should see headers like x-ratelimit-limit, x-ratelimit-remaining
-    //console.log(res.headers);
+    // console.log(res.headers);
     const body = res.json();
     if (res.status !== 200)
         throw new Error(
@@ -40,28 +33,21 @@ const getFileBySha = async (owner, repo, sha, filePath) => {
 };
 
 const getFileCommitSha = async (owner, repo, branch, filePath) => {
-    const headers = { Accept: "application/vnd.github.v3+json" };
-    if (
-        config.GITHUB_OAUTH_APP_CLIENT_ID &&
-        config.GITHUB_OAUTH_APP_CLIENT_SECRET
-    ) {
-        headers["Authorization"] =
-            "Basic " +
-            Buffer.from(
-                config.GITHUB_OAUTH_APP_CLIENT_ID +
-                ":" +
-                config.GITHUB_OAUTH_APP_CLIENT_SECRET,
-            ).toString("base64");
+    const headers = { Accept: 'application/vnd.github.v3+json' };
+    if (config.GITHUB_OAUTH_APP_CLIENT_ID && config.GITHUB_OAUTH_APP_CLIENT_SECRET) {
+        headers.Authorization = `Basic ${Buffer.from(
+            `${config.GITHUB_OAUTH_APP_CLIENT_ID}:${config.GITHUB_OAUTH_APP_CLIENT_SECRET}`,
+        ).toString('base64')}`;
     }
     const fileRes = await fetch(
         `${GITHUB_API}/repos/${owner}/${repo}/commits?sha=${branch}&path=${filePath}`,
         {
-            method: "GET",
-            headers: headers,
+            method: 'GET',
+            headers,
         },
     );
     // This can be useful to check auth. You should see headers like x-ratelimit-limit, x-ratelimit-remaining
-    //console.log(fileRes.headers);
+    // console.log(fileRes.headers);
     const fileBody = await fileRes.json();
     if (fileRes.status !== 200)
         throw new Error(
